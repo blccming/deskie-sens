d8c64edec21ff3936f32da86e84bb8a3bb1dabde
bloom <115806543+blccming@users.noreply.github.com>
2025-12-30 23:36:48 +0100

    added reading response from ld2410c when submitting commands

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ from peripherals.HLK_LD2410C import LD2410C
 radar = LD2410C()
 radar.init()
 
-radar.restart()
-
 while True:
     pass
--- peripherals/HLK_LD2410C.py
+++ peripherals/HLK_LD2410C.py
@@ -1,3 +1,4 @@
+import re
 from time import sleep
 
 import serial
@@ -11,6 +12,7 @@ class LD2410C:
     TARGET_STATE_BACKGROUND_NOISE_DETECTION = 0x04
     TARGET_STATE_BACKGROUND_NOISE_DETECTION_SUCCESS = 0x05
     TARGET_STATE_BACKGROUND_NOISE_DETECTION_FAIL = 0x06
+    TARGET_STATE_UNDEFINED = 0xFF
 
     def __init__(self):
         self.__ser = serial.Serial(
@@ -21,30 +23,29 @@ class LD2410C:
             stopbits=serial.STOPBITS_ONE,
             parity=serial.PARITY_NONE,
         )
-        self.__target_stat: int = 0
+        self.__target_stat: int = TARGET_STATE_UNDEFINED
         self.__presence: bool = False
 
     def init(self):
         self.__ser.flush()
-        # TODO: radar config here
 
-    # TODO: check response
-    def enable_config(self) -> bool:
-        return self.config_transceive(0x00FF, 0x0001)
-
-    # TODO: check response
-    def disable_config(self) -> bool:
-        return self.config_transceive(0x00FE, None)
-
-    # TODO: check response
-    def config_reset(self) -> bool:
-        return self.config_transceive(0x00A2, None)
-
-    # TODO: check response
-    def restart(self) -> bool:
         self.enable_config()
-        sleep(0.02)
-        return self.config_transceive(0x00A3, None)
+
+        # TODO: radar config here (with error handling)
+
+        self.disable_config()
+
+    def enable_config(self) -> bool:
+        return self.config_transceive(0x00FF, 0x0001) == "0800ff01000001004000"
+
+    def disable_config(self) -> bool:
+        return self.config_transceive(0x00FE, None) == "0400fe010000"
+
+    def factory_reset(self) -> bool:
+        return self.config_transceive(0x00A2, None) == "0400a2010000"
+
+    def restart(self) -> bool:
+        return self.config_transceive(0x00A3, None) == "0400a3010000"
 
     # automatic background noise detection and sensitivity configuration
     def auto_sensitivity(self, measurement_duration) -> bool:
@@ -58,7 +59,7 @@ class LD2410C:
 
     # STRUCTURE
     # | HEADER | DATA_LENGTH | DATA: CMD WORD | DATA: CMD VALUE | FOOTER |
-    def config_transceive(self, cmdwrd: int, cmdval: int | None) -> bool:
+    def config_transceive(self, cmdwrd: int, cmdval: int | None) -> str:
         # header and footer are the same for every configuration command
         HEADER = bytearray([0xFD, 0xFC, 0xFB, 0xFA])
         FOOTER = bytearray([0x04, 0x03, 0x02, 0x01])
@@ -66,12 +67,11 @@ class LD2410C:
         # convert integer input to bytes (-> LSB first)
         cmdwrd = cmdwrd.to_bytes(2, byteorder="little")
 
-        cmdval_byte_length = cmdval.bit_length() // 8 + (
-            1 if cmdval.bit_length() % 8 != 0 else 0
-        )
-        cmdval = (
-            cmdval.to_bytes(cmdval_byte_length, byteorder="little") if cmdval else None
-        )
+        if cmdval:
+            cmdval_byte_length = cmdval.bit_length() // 8 + (
+                1 if cmdval.bit_length() % 8 != 0 else 0
+            )
+            cmdval = cmdval.to_bytes(cmdval_byte_length, byteorder="little")
 
         # calculate length of data intra-frame
         data_length = len(cmdwrd) + (len(cmdval) if cmdval else 0)
@@ -90,8 +90,15 @@ class LD2410C:
         print(f"Sending config frame: {cmd_packet.hex()}")
         self.__ser.write(cmd_packet)
 
-        # receive ACK; TODO: implement ACK handling
-        print(f"Received: {self.__ser.read(16).hex()}")
+        # receive ACK
+        answer = self.__ser.read(32).hex()
+        print(f"Received: {answer}")
         self.__ser.flush()
 
-        return True
+        # get clean answer to return
+        try:
+            answer_clean = re.search(r"(?<=fdfcfbfa).*?(?=04030201)", answer).group(0)
+        except AttributeError:
+            answer_clean = None
+
+        return answer_clean
